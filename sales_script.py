485101403637ada9ef555f487551213bfcd859ea
kusn-odoo <107050492+kusn-odoo@users.noreply.github.com>
2022-07-06 11:30:37 -0400

    Update sales_script.py
    Updated script to group by sales person

--- sales_script.py
+++ sales_script.py
@@ -1,28 +1,21 @@
 from xmlrpc import client
-# password = "7f25f13bcb3efa3760757e25ff3ef820e7ad58ab"
-# username = "kusn" #the user
-# dbname = 'openerp'    #the database
-# sock = client.ServerProxy('http://www.odoo.com/xmlrpc/')
-# print(sock.version())
-
 
 HOST = "www.odoo.com" # Host url name
 db =  "openerp"  # Name of the database
 USER = "kusn" # User Name to login database
 password = "" # Password to login database
-url = 'https://%s/xmlrpc/2/' % (HOST)
-common_proxy = client.ServerProxy(url+'common')
-object_proxy = client.ServerProxy(url+'object')
-uid = common_proxy.login(db,USER,password)
+url = 'https://%s/xmlrpc/2/' % (HOST) # URL to connect to
+common_proxy = client.ServerProxy(url+'common') # Proxy for common connection
+object_proxy = client.ServerProxy(url+'object') # Proxy for object connection
+uid = common_proxy.login(db,USER,password) # User id
 print("Logged in as %s (uid:%d)" % (USER, uid))
 # working to retrieve records
-ids = object_proxy.execute_kw(db, uid, password, 'sale.order', 'search', [[]], {'limit':5})
-print(ids)
-# record = object_proxy.execute_kw(db, uid, password, 'sale.order', 'read', [ids], {'fields': ['user_id', 'partner_id']})
-# print(record)
-
+ids = object_proxy.execute_kw(db, uid, password, 'res.users', 'search', [[('sale_team_id','!=',False)]], {})
+# print(ids)
+print(len(ids))
+user_ids = []
 # groupby sales person
-x =[]
-print(isinstance(x,(list,tuple)))
-record = object_proxy.execute_kw(db, uid, password, 'sale.order', 'read_group', [[], ['user_id'],['date_order']])
-print(record[0])
+record = object_proxy.execute_kw(db, uid, password, 'sale.order', 'read_group', [[('user_id','in',ids),('state','=','sale')], ['user_id','amount_total'],['user_id']],{"offset":0 ,"orderby":'amount_total desc', "lazy":True,"limit":10})
+for rec in record:
+    print(rec['user_id'],rec['amount_total'])
+
