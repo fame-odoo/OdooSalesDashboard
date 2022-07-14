7a9c59d5a20bcb3cdb7447f5fc78839665d9b785
kusn-odoo <107050492+kusn-odoo@users.noreply.github.com>
2022-07-14 14:58:06 -0400

    Update api_oauth.js

--- api_oauth.js
+++ api_oauth.js
@@ -8,12 +8,13 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = 'token.json';
-
+const PROPERTIES_PARH='properties.json'
+const PROPERTIES =JSON.parse(fs.readFileSync(PROPERTIES_PARH))
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), listMajors);
+  authorize(JSON.parse(content), retrieve_overall_info_sales);
 });
 
 /**
@@ -29,8 +30,12 @@ function authorize(credentials, callback) {
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
+  
+    
+    // console.log(JSON.parse(fileproperties).sheet_id)
     if (err) return getNewToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
+    
     callback(oAuth2Client);
   });
 }
@@ -71,22 +76,49 @@ function getNewToken(oAuth2Client, callback) {
  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-function listMajors(auth) {
+   async function retrieve_overall_info_sales(auth) {
   const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '17a5-5E1nqL_ZF5sxI2hLhPBYv_XKzWI_r2ugEnT0Pek',
-    range: 'US-REV!A2:E',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.values;
+  var years = PROPERTIES.sheet_id
+
+  let SHEET_ID=''
+  let overall_data_every_year = {}
+  for(var year of Object.keys(years)){
+    let yearly_data = await makeapicall(years,year,sheets)
+    console.log(yearly_data)
+    overall_data_every_year[year]=yearly_data
+}
+console.log(overall_data_every_year)
+}
+async function makeapicall(years,year,sheets){
+
+    SHEET_ID = years[year]
+
+  let values = (await sheets.spreadsheets.values.get({
+    spreadsheetId: SHEET_ID,
+    range: 'US-DSBU!A3:K',
+  })).data;
+  const rows = values.values;
+  let overall_data ={}
     if (rows.length) {
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
       rows.map((row) => {
-        console.log(`${row[0]}, ${row[4]}`);
+        if(row[0]){
+        overall_data[row[0]]={
+          "actual_mrr":row[2],
+          "actual_nrr":row[3],
+          "target_mrr":row[4],
+          "target_nrr":row[5],
+          "achieve_mrr":row[6],
+          "achieve_nrr":row[7],
+          "new_ent_sold":row[10]
+        }
+      }
       });
+      console.log("sdfsdf",year)
+  
     } else {
       console.log('No data found.');
     }
-  });
+  // console.log(overall_data)
+  
+  return overall_data
 }
